0a60819a4880426b3a00eac1a00481831901e5d3
FaisalFehad <FaisalFehad>
2016-06-28 09:42:40 +0000

    Remove unnecessary logon spec and change button name-fixes

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -12,12 +12,10 @@ feature "Articles can be posted and edited" do
 
 
   scenario 'Visit the home page, post an article then edit it' do
-    # Sign up first
+    # Sign up first (Will be loged in after sign up)
     user.sign_up
-    # Login
-    user.sign_in
     # Create an article first
-    article_form.visit_home_page.create_an_article
+    article_form.create_an_article
     # Then Edits the article
     article_form.edit_an_article
     # Go back to the home page to verify the changes
