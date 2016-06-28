4fff3760b755e603f9658f8620e591abc2e639f3
FaisalFehad <faisalfehad@gmail.com>
2016-06-28 09:42:40 +0000

    Remove unnecessary logon spec and change button name-fixes

--- spec/support/User.rb
+++ spec/support/User.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 class User
   include Capybara::DSL
 
-
   def sign_up
     visit('/users/sign_up/')
     fill_in('Email', with:'agmin@agmin.com')
     fill_in('Password', with:'Password12')
+    fill_in('Password confirmation', with:'Password12')
     click_on('Sign up')
   end
 
@@ -15,11 +15,11 @@ class User
     visit('/users/sign_in/')
     fill_in('Email', with:'agmin@agmin.com')
     fill_in('Password', with:'Password12')
-    click_on('Login')
+    click_on('Log in')
   end
 
   def logout
-    visit('/')
+    visit('/users/sign_in_out/')
     click_on('Logout')
   end
 end
