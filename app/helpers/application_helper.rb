1d6e9de97fc981359129fd6a0b7a419f70927c8c
FaisalFehad <FaisalFehad>
2016-06-29 15:59:52 +0000

    Render article body in Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+def markdown(content)
+  renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+    options = {
+      autolink: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true,
+      no_images: true }
+
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+  end
 end
