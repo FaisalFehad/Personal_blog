3d45059b9b4a9f9d32c124998bcf7100cafe62f0
FaisalFehad <FaisalFehad>
2016-07-03 11:50:19 +0000

    Fix production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,18 +79,13 @@ Rails.application.configure do
 
   # Sendgrid/Heroku
   config.action_mailer.default_url_options = {  host: "https://personal-blog22.herokuapp.com " }
-  config.action_mailer.delivery_method = :smtp
-
-  Mail.defaults do
-  delivery_method :smtp, {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings   = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    authentication:       :plain,
+    user_name:            'app52905832@heroku.com',
+    password:             'i1jqf0il2902',
+    domain:               'heroku.com',
+    enable_starttls_auto:  true
   }
-end
-
-end
