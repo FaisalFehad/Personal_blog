31ba0f0441c02968c73d382c09d8dcdc48bba59d
FaisalFehad <FaisalFehad>
2016-07-03 11:41:05 +0000

    Sendgrid

--- app/mailers/article_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ArticleMailer < ActionMailer::Base
-
-  def article_created(follower, article)
-    @article = article
-    follower.each do |f|
-      mail to: f.email, from:'no_reply@gmail.com', subject: "An article has been created"
-    end
-  end
-end
