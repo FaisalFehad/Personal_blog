79f273d802d02d73c76c429810244855b8c6630d
FaisalFehad <FaisalFehad>
2016-07-03 19:41:30 +0000

    Fix typo

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -14,7 +14,7 @@ def send_article_created_email(follower, article)
     @article = article
     follower.each do |f|
       mail to: f.email,
-           subject: "An article has been created",
+           subject: "A article has been created",
            from:'app52905832@heroku.com'
     end
   end
