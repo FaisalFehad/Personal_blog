ba3a191f887ede77c14279584c423d0d281df59a
FaisalFehad <faisalfehad@gmail.com>
2016-07-05 10:07:27 +0000

    Users cant sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :articles
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,4 +26,4 @@ Rails.application.configure do
     domain:               'heroku.com',
     enable_starttls_auto:  true
   }
-end 
+end
