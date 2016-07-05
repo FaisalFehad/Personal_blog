85ea3619370e6c025c598baa1cf56a018a3a4366
FaisalFehad <faisalfehad>
2016-07-05 10:40:16 +0000

    Remove edit profile link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   has_many :articles
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
