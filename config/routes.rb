ae0e77715ca0e01cdfb7e503fcec807db6c9d93f
FaisalFehad <faisalfehad@gmail.com>
2016-07-05 09:53:57 +0000

    Specify resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'followers/:id/unfollow', to: 'followers#destroy', as: 'unfollow'
-
   devise_for :users
   root 'articles#index'
-  resources :followers
+  resources :followers, only: [:new, :create, :destroy]
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
 end
