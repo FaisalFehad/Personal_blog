03a19d53d4b1157300006256d8eb2f615448c641
FaisalFehad <faisalfeahd@gmail.com>
2016-07-07 21:10:03 +0100

    Uplode image with PaperClip

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,18 @@
 class Article < ActiveRecord::Base
+  # association
   has_many :comments, dependent: :destroy
   belongs_to :user
+  # FriendlyId
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
+  # validation
   validates :title,   presence: true, length: {in: 2..45 }
   validates :content, presence: true, length: {in: 3..2000}
+  # paperclip
+  has_attached_file :image, styles: { article: "700x400>", thumb: "100x100>" },
+   default_url: "/images/:style/missing.png"
+  #validates_attachment_content_type :article, content_type: /\Aimage\/.*\Z/
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+
 end
