7f834443e0be8c975480dcf8939b1e5f49eac297
FaisalFehad <faisalfehad@gmail.com>
2016-07-07 13:41:51 +0100

    Delete comments with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
