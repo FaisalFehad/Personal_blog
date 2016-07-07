b664997d0a05cd6af65b734bf37cffa01ee1393a
FaisalFehad <faisalfeahd@gmail.com>
2016-07-07 13:41:51 +0100

    Delete comments with articles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
-  validates :name,    presence: true
+  validates :name, presence: true
   validates :body, presence: true, length: { maximum: 250 }
 end
